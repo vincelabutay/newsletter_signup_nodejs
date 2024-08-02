965688fe8ebeb383bb55b7545fbfa073ff6cefd0
vincelabutay <vincelabutay@gmail.com>
2024-08-02 20:55:35 +0800

    done

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ app.post("/", function(req, res){
     }
     
     const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -54,6 +61,13 @@ app.post("/", function(req, res){
     request.end();
 
 });
+
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+
 
 
 app.listen(3000, function(){
